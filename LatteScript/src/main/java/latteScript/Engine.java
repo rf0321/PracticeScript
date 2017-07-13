900d8c345d60d8a4705e846e5cb82c2fd0bf9f39
ItinoseSan <jkekun@gmail.com>
2017-07-13 16:34:58 +0900

    エンジンクラスをしっかり書いた

--- LatteScript/src/main/java/latteScript/Engine.java
+++ LatteScript/src/main/java/latteScript/Engine.java
@@ -2,16 +2,8 @@ package latteScript;
 import  latteScript.expression.IExpression;
 import  latteScript.expression.Operator;
 import  latteScript.expression.ImmutableValue;
-import  latteScript.operator.IOperator;
+import latteScript.operator.*;
 import  latteScript.expression.Operator;
-import  latteScript.operator.Addtion;
-import  latteScript.operator.Multipl;
-import  latteScript.operator.Sub;
-import  latteScript.operator.SetValue;
-import  latteScript.operator.CalculateAuthOp;
-import  latteScript.operator.WhileOperator;
-import  latteScript.operator.GetOperator;
-import  latteScript.operator.Equal;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,6 +12,18 @@ import java.util.Map;
 public class Engine {
     public Map<String, IOperator> operators = new HashMap<String, IOperator>();
     public Map<String, Object> variables = new HashMap<String, Object>();
+
+    public Engine(){
+        operators.put("+",new Addtion());
+        operators.put("*",new Multipl());
+        operators.put("-",new Sub());
+        operators.put("%",new Division());
+        operators.put("=",new Equal());
+        operators.put("val",new SetValue());
+        operators.put("pointer",new GetOperator());
+        operators.put("until",new WhileOperator());
+        operators.put("step",new CalculateAuthOp());
+    }
 
     public Object eval(Object script){
         return getExpression(script).eval(this);
@@ -35,4 +39,3 @@ public class Engine {
         }
     }
 }
-
